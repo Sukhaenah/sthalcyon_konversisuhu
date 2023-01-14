8b50c9db0efc945aa1ddb7ee27d984680ea99fdd
Sukhaenah <100648586+Sukhaenah@users.noreply.github.com>
2023-01-14 13:10:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import sys
 import option_menu
 sys.path.insert(1, "C:/app/sthalcyon_konversisuhu/main.py/streamlit_option_menu")
-from streamlit_option_menu 
+from streamlit_option_menu import option_menu
 
 # Navigasi sidebar
 with st.sidebar :
